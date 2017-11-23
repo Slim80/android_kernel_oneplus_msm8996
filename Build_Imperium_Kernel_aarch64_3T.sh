e5da0df69ce2e55a939c899976355d609713fc02
Slim80 <bestboss1980@gmail.com>
2017-11-23 22:33:41 +0100

    add Imperium Kernel scripts

---
+++ Build_Imperium_Kernel_aarch64_3T.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+export KERNELDIR=`readlink -f .`
+export PARENT_DIR=`readlink -f ..`
+export ARCH=arm64
+export SUBARCH=arm64
+export PATH=/home/slim80/Scrivania/Kernel/Compilatori/aarch64-linux-android-4.9/bin:$PATH
+#export PATH=/home/slim80/Scrivania/Kernel/Compilatori/DespairFactor-aarch64-linux-android-4.9/bin:$PATH
+export CROSS_COMPILE=aarch64-linux-android-
+export KCONFIG_NOTIMESTAMP=true
+
+IMPERIUM="/home/slim80/Scrivania/Kernel/oneplus/Imperium"
+KERNELDIR="/home/slim80/Scrivania/Kernel/oneplus/Imperium/Imperium_Kernel_O"
+BUILDKERNEL="/home/slim80/Scrivania/Kernel/oneplus/Imperium/Build_Kernel"
+FINALKERNEL="/home/slim80/Scrivania/Kernel/oneplus/Imperium/Final_Kernel"
+IMAGE="/home/slim80/Scrivania/Kernel/oneplus/Imperium/Imperium_Kernel_O/arch/arm64/boot"
+ANYKERNEL="/home/slim80/Scrivania/Kernel/oneplus/Imperium/Imperium_Kernel_O/AnyKernel"
+NUM_CPUS=`grep -c ^processor /proc/cpuinfo`
+VERSION=1.0-beta1
+
+find -name '*.gz*' -exec rm -rf {} \;
+find -name '*.dtb' -exec rm -rf {} \;
+find -name '*.ko' -exec rm -rf {} \;
+rm -rf $ANYKERNEL/zImage
+
+rm -rf ~/.ccache
+ccache -C
+
+make ARCH=arm64 imperium_defconfig || exit 1
+
+make -j$NUM_CPUS || exit 1
+
+cp arch/arm64/boot/Image.gz-dtb $ANYKERNEL/zImage
+
+rm -rf imperium_install
+mkdir -p imperium_install
+make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} -j4 INSTALL_MOD_PATH=imperium_install INSTALL_MOD_STRIP=1 modules_install
+find imperium_install/ -name '*.ko' -type f -exec cp '{}' $ANYKERNEL/modules/ \;
+
+cd $ANYKERNEL
+zip -r $BUILDKERNEL/Slim80/kernel/Imperium_Kernel.zip .
+
+cd $BUILDKERNEL
+zip -r Imperium_Kernel_OP3T_OSS_Oreo_v$VERSION.zip .
+ 
+mv ./Imperium_Kernel_OP3T_OSS_Oreo_v* $FINALKERNEL
+
+echo "* Done! *"
+echo "* Imperium Kernel v$VERSION is ready to be flashed *"
---
+++ clean.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+export KERNELDIR=`readlink -f .`
+export PARENT_DIR=`readlink -f ..`
+export ARCH=arm64
+export SUBARCH=arm64
+export PATH=/home/slim80/Scrivania/Kernel/Compilatori/aarch64-linux-android-4.9/bin:$PATH
+export CROSS_COMPILE=aarch64-linux-android-
+export KCONFIG_NOTIMESTAMP=true
+
+IMPERIUM="/home/slim80/Scrivania/Kernel/oneplus/Imperium"
+KERNELDIR="/home/slim80/Scrivania/Kernel/oneplus/Imperium/Imperium_Kernel_O"
+BUILDKERNEL="/home/slim80/Scrivania/Kernel/oneplus/Imperium/Build_Kernel"
+FINALKERNEL="/home/slim80/Scrivania/Kernel/oneplus/Imperium/Final_Kernel"
+IMAGE="/home/slim80/Scrivania/Kernel/oneplus/Imperium/Imperium_Kernel_O/arch/arm64/boot"
+ANYKERNEL="/home/slim80/Scrivania/Kernel/oneplus/Imperium/Imperium_Kernel_O/AnyKernel"
+
+rm -f arch/arm64/boot/*.cmd
+rm -f arch/arm64/boot/dts/*.cmd
+rm -f arch/arm64/boot/Image*.*
+rm -f arch/arm64/boot/.Image*.*
+find -name '*.gz*' -exec rm -rf {} \;
+find -name '*.dtb' -exec rm -rf {} \;
+find -name '*.ko' -exec rm -rf {} \;
+rm -rf $ANYKERNEL/zImage
+
+make clean
+make distclean
+rm -rf ~/.ccache
+ccache -C
