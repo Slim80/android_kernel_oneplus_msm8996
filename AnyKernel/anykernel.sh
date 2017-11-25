7c79df770eb9d40928c0963890a5dbc798a0b20d
Slim80 <bestboss1980@gmail.com>
2017-11-25 18:19:16 +0100

    update AnyKernel2

--- AnyKernel/anykernel.sh
+++ AnyKernel/anykernel.sh
@@ -9,6 +9,7 @@ do.devicecheck=0
 do.initd=0
 do.modules=1
 do.cleanup=1
+do.cleanuponabort=0
 device.name1=oneplus3t
 device.name2=OnePlus3T
 device.name3=OnePlus 3T
@@ -17,15 +18,21 @@ device.name3=OnePlus 3T
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
+ramdisk_compression=auto;
 
+
+## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . /tmp/anykernel/tools/ak2-core.sh;
 
+
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
 chmod -R root:root $ramdisk/*;
 chmod 775 $ramdisk/sbin
 chmod 755 $ramdisk/sbin/busybox
+
 
 ## AnyKernel install
 dump_boot;
