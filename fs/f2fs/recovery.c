5d5fabbd44e3df76304066db8ac1fae592fe2731
Yunlei He <heyunlei@huawei.com>
2018-07-19 14:57:14 +0800

    f2fs: fix wrong kernel message when recover fsync data on ro fs
    This patch fix wrong message info for recover fsync data
    on readonly fs.
    
    Signed-off-by: Yunlei He <heyunlei@huawei.com>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/recovery.c
+++ fs/f2fs/recovery.c
@@ -583,7 +583,8 @@ int recover_fsync_data(struct f2fs_sb_info *sbi, bool check_only)
 	bool need_writecp = false;
 
 	if (s_flags & MS_RDONLY) {
-		f2fs_msg(sbi->sb, KERN_INFO, "orphan cleanup on readonly fs");
+		f2fs_msg(sbi->sb, KERN_INFO,
+				"recover fsync data on readonly fs");
 		sbi->sb->s_flags &= ~MS_RDONLY;
 	}
 
